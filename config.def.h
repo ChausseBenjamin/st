7af6dfa01bc98f3f00adabb7c2034c50e7680d24
Benjamin Chausse <benjamin@chausse.xyz>
2020-10-16 00:13:38 -0400

    Keep fonts semi-standardized

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Apercu Nerd Font:pixelsize=16:antialias=true:autohint=true";
+static char *font = "monospace:pixelsize=16:antialias=true:autohint=true";
 static char *font2[] = { "Spleen 32x64:pixelsize=15:antialias=true:autohint=true" };
 static int borderpx = 2;
 
